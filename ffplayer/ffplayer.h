a4d552ba966f0ee3067bf21e8e3e1ff9b144aeea
dpeng <df.peng@hotmail.com>
2017-02-20 16:18:42 +0800

    init the project with VS2015

---
+++ ffplayer/ffplayer.h
@@ -0,0 +1,32 @@
+
+// ffplayer.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CffplayerApp:
+// See ffplayer.cpp for the implementation of this class
+//
+
+class CffplayerApp : public CWinApp
+{
+public:
+	CffplayerApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CffplayerApp theApp;
